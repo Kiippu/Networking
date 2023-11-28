b61b5abee2754e87c38e75b239601f0e1b6597c7
Kory Smith <kosa.smit@gmail.com>
2023-11-28 23:07:21 +1100

    set up git and start network tuts

---
+++ src/simple_server/simple_server.h
@@ -0,0 +1,10 @@
+/**
+ * @file simple_server.h
+ * @author kory smith
+ * @brief following this video https://www.youtube.com/watch?v=W9b9SaGXIjA&list=PLhnN2F9NiVmAMn9iGB_Rtjs3aGef3GpSm&index=2
+ * @version 0.1
+ * @date 2023-11-28
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
